46f485f63b323b97b121a072b0025499a782b24a
ei-bro <eibrahim1748@gmail.com>
2022-02-22 00:33:22 +0300

    some word change

--- src/component/CounterDisplayer.js
+++ src/component/CounterDisplayer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counterdisplayer extends Component {
     render() {
-        return <h1>All click counter : {this.props.value}</h1>;
+        return <h1>All Click counter : {this.props.value}</h1>;
     }
 }
 
--- src/component/EvenCounterDisplayer.js
+++ src/component/EvenCounterDisplayer.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class EvenCounterDisplayer extends Component {
     render() {
-        return <h1>All click counter : {this.props.value}</h1>;
+        return <h1>Even Clicks Counter : {this.props.value}</h1>;
     }
 }
